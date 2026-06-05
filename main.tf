81923ea16017ccf2f743719bd8e83e058f6c5b3a
gauravtiwari2101 <gauravt2101@gmail.com>
2026-06-05 14:37:37 +0530

    IAC

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform{
+    required_providers{
+        local={
+            source="hashicorp/local"
+            version= "~>2.5"
+        }
+    }
+}
+
+resource "local_file""student_doc"{
+    filename="${path.module}/hello_terrraform.txt"
+    content="Welcome to Terraform! You successfully built this file locally on windows."
+}
